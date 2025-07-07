67d00237018f6d44d022f40d8cd2aaacac68bcb6
bqka <aditya2005mendiratta@gmail.com>
2025-07-07 23:52:33 +0530

    commit

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'url'
 import { dirname, resolve } from 'path'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
+import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -10,6 +12,7 @@ const __dirname = dirname(__filename)
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     viteStaticCopy({
       targets: [
         { src: 'public/manifest.json', dest: '.' },
@@ -18,12 +21,17 @@ export default defineConfig({
       ]
     })
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, "src")
+    }
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        popup: resolve(__dirname, 'popup.html') // This tells Vite to emit popup.html
+        popup: resolve(__dirname, 'popup.html')
       },
       output: {
         entryFileNames: '[name].js',
