f51a188111b0b8782490fd78e1dad50665523e30
bqka <aditya2005mendiratta@gmail.com>
2025-07-08 02:32:21 +0530

    debugging 4

--- webpage/api/index.ts
+++ webpage/api/index.ts
@@ -21,6 +21,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  if (req.query.path) {
+    req.url = '/' + req.query.path;
+  }
+  console.log('=== REQUEST DEBUG ===');
+  console.log('Method:', req.method);
+  console.log('URL:', req.url);
+  console.log('Query path:', req.query.path);
+  next();
+});
+
 // ---- Routes ----
 app.post("/api/summarize", async (req: Request, res: Response) => {
   const { text } = req.body;
